7dd28f94bea39e6f0908bfe971bce935a2838f83
Maiketon <miky_lee24@hotmail.com>
2021-12-02 18:32:17 -0600

    Datos a Validar
    JS

--- js/validar.js
+++ js/validar.js
@@ -6,9 +6,21 @@ evento.preventDefault();
 })
     
     const datos = {
+        boleta: '',
         nombre: '',
+        AP: '',
+        AM: '',
+        fechanac:'',
+        curp: '',
+
+        calle:'',
+        nunmcalle:'',
+        colonia:'',
+        alcaldia:'',
+        cp:'',
+        tel: '',
         email: '',
-        curp: ''
+
     }
     
     // submit
@@ -27,7 +39,7 @@ evento.preventDefault();
     
         const { nombre, email } = datos;
     
-        if(nombre === '' || email === '' || crup === '' ) {
+        if(nombre === '' || email === ''  ) {
             console.log('Al menos un campo esta vacio');
             mostrarError('Todos los campos son obligatorios');
             return; // Detiene la ejecución de esta función
@@ -44,15 +56,27 @@ evento.preventDefault();
 
     
     // Eventos de los Inputs...
+
+    //Identidad//
+    const boleta = document.querySelector('#boleta');
     const nombre = document.querySelector('#nombre');
+    const AP = document.querySelector('#apellidop');
+    const AM = document.querySelector('#apellidom');
+    const fechanac = document.querySelector('#fechanac');
+    const curp =document.querySelector('#curp');
+
+    //Contacto//
+    const calle = document.querySelector('#Calle');
+    const nunmcalle = document.querySelector('#numcalle');
+    const colonia = document.querySelector('#Col');
+    const alcaldia = document.querySelector("alcaldia");
+    const cp = document.querySelector('#cp');
+    const tel = document.querySelector('#tel');
     const email = document.querySelector('#email');
-    const curp = document.querySelector('#curp');
-
-
+    
     
     nombre.addEventListener('input', leerTexto);
     email.addEventListener('input', leerTexto);
-    curp.addEventListener('input', leerTexto);
     
     
     function leerTexto(e) {
