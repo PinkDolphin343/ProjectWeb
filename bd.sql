e35a35b78c83f40b3c28e9e71a3441cd67acd89d
Rafael Hayyim Medina Sosa <61524008+RHMS-bot@users.noreply.github.com>
2021-12-06 12:10:51 -0600

    Base del proyecto

---
+++ bd.sql
@@ -0,0 +1,11 @@
+create database pruebas;
+use pruebas;
+create table usuarios(
+				id int auto_increment,
+                nombre varchar(50),
+                apellido varchar(50),
+                usuario varchar(50),
+                password varchar(150),
+                primary key(id)
+					);
+select * from usuarios;
